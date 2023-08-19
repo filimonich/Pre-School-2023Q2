265f6b0e22bb07508d4ed59c1cc314cf0d029922
Sergey Filimonov <srfilimonoff@gmail.com>
2023-08-19 22:45:49 +0300

    feat: add slider switching by inputs
    fix: disabled overflow and disabled vertical scrolling, align the heder

--- library/src/blocks/favorites/favorites.js
+++ library/src/blocks/favorites/favorites.js
@@ -1,4 +1,33 @@
 (() => {
+  document.addEventListener('DOMContentLoaded', () => {
+    // Установка начальной высоты блока избранного
+    const initialHeight = document.querySelector('.favorites__contents').offsetHeight;
+    document.querySelector('.favorites__contents').style.height = `${initialHeight}px`;
+  
+    // Функция для затухания текущих карточек и проявления новых карточек
+    const changeCards = (season) => {
+      // Затухание текущих карточек
+      const currentCards = document.querySelector(
+        '.favorites__holder:not([style*="display: none"])'
+      );
+      currentCards.style.display = 'none';
+      // Проявление новых карточек
+      const newCards = document.querySelectorAll('.favorites__holder')[season];
+      newCards.style.display = 'block';
+    };
+  
+    // Обработка нажатий на кнопки панели навигации
+    const buttons = document.querySelectorAll('.favorites__input-button');
+    buttons.forEach((button) => {
+      button.addEventListener('click', () => {
+        // Получение индекса нажатой кнопки
+        const season = Array.prototype.indexOf.call(buttons, button);
+        // Изменение карточек
+        changeCards(season);
+      });
+    });
+  });
+  
 
-
+  
 })();

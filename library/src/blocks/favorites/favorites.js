d8302fff1c2d61ec208bb045f3d7ecc768dcb74e
Sergey Filimonov <srfilimonoff@gmail.com>
2023-08-25 15:53:37 +0300

    fix: update the height of favorites__contents and the position of favorites__radio

--- library/src/blocks/favorites/favorites.js
+++ library/src/blocks/favorites/favorites.js
@@ -1,11 +1,12 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
     // Установка начальной высоты блока избранного
-    const initialHeight = document.querySelector('.favorites__contents').offsetHeight;
-    document.querySelector('.favorites__contents').style.height = `${initialHeight}px`;
-  
+    // const initialHeight = document.querySelector('.favorites__contents').offsetHeight;
+    // document.querySelector('.favorites__contents').style.height = `${initialHeight}px`;
+
+
     // Функция для затухания текущих карточек и проявления новых карточек
-    const changeCards = (season) => {
+    const changeCards = season => {
       // Затухание текущих карточек
       const currentCards = document.querySelector(
         '.favorites__holder:not([style*="display: none"])'
@@ -15,10 +16,10 @@
       const newCards = document.querySelectorAll('.favorites__holder')[season];
       newCards.style.display = 'block';
     };
-  
+
     // Обработка нажатий на кнопки панели навигации
     const buttons = document.querySelectorAll('.favorites__input-button');
-    buttons.forEach((button) => {
+    buttons.forEach(button => {
       button.addEventListener('click', () => {
         // Получение индекса нажатой кнопки
         const season = Array.prototype.indexOf.call(buttons, button);
@@ -27,7 +28,4 @@
       });
     });
   });
-  
-
-  
 })();

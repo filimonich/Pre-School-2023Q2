ed84bc15e7c54ea53cb79b115ac09892f13fd3ec
Sergey Filimonov <srfilimonoff@gmail.com>
2023-08-27 21:07:59 +0300

    feat: add modal register

--- library/src/js/blocks.js
+++ library/src/js/blocks.js
@@ -3,3 +3,4 @@ import "../blocks/hamburger/hamburger.js";
 import "../blocks/about/about.js";
 import "../blocks/favorites/favorites.js";
 import "../blocks/header/header.js";
+import "../blocks/modal/modal.js";

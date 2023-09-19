e6d72d1f77e58aee8506ee1123726f25d9b452f3
Sergey Filimonov <srfilimonoff@gmail.com>
2023-09-19 20:34:02 +0300

    feat: make a working assembly

---
+++ audio-player/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import injectHtml from 'vite-plugin-html-inject';
+import sassGlobImports from 'vite-plugin-sass-glob-import'; 
+
+export default defineConfig({
+  base: '',
+  root: './src',
+  publicDir: './../public',
+  build: {
+    outDir: './../docs',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+      },
+    },
+  },
+
+  assetsInclude: ['**/*.html', '**/*.css'],
+
+  plugins: [injectHtml(), sassGlobImports()],
+});

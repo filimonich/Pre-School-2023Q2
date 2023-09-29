f7abb504e94332b5d1608bcb78f0695b8d8b9d45
Sergey Filimonov <srfilimonoff@gmail.com>
2023-09-29 18:59:04 +0300

    feat: handle errors and update preloader

--- image-galery/src/js/unsplashAPI.js
+++ image-galery/src/js/unsplashAPI.js
@@ -4,29 +4,50 @@ for (let i = 1; i < 6; i++) {
   }, i * 2000);
 }
 
-// поле ввода
 const input = document.getElementById('search-input');
+const gallery = document.querySelector('.galery__contents');
 
 // массив со случайными запросами
 const randomRequests = [
   'spring',
-  // 'summer',
-  // 'autumn',
-  // 'winter',
-  // 'nature',
-  // 'city',
-  // 'people',
-  // 'animals',
+  'summer',
+  'autumn',
+  'winter',
+  'nature',
+  'city',
+  'people',
+  'animals',
 ];
 
 // получения данных от api
 const getData = async url => {
-  // делаем запрос к api
-  const res = await fetch(url);
-  // преобразуем ответ в json
-  const data = await res.json();
-  // возвращаем результаты
-  return data.results;
+  try {
+    // делаем запрос к api
+    const res = await fetch(url);
+
+    // проверяем, успешен ли запрос
+    if (!res.ok) {
+      throw new Error(`Ошибка: ${res.status}`);
+    }
+
+    // преобразуем ответ в json
+    const data = await res.json();
+
+    // возвращаем результаты
+    return data.results;
+  } catch (error) {
+    console.error('An error occurred while receiving data:', error);
+
+    gallery.innerHTML = ''; // очищаем галерею
+
+    const errorMessage = document.createElement('p');
+    errorMessage.className = 'galery__text';
+    errorMessage.innerHTML =
+      'An error occurred while receiving the data. Please try again. Произошла ошибка при получении данных. Пожалуйста, попробуйте еще раз.';
+    gallery.appendChild(errorMessage);
+
+    throw error; // Перебрасываем ошибку, чтобы остановить выполнение функции
+  }
 };
 
 // функция для создания div для изображения
@@ -37,20 +58,21 @@ const createImageDiv = () => {
   return imgDiv;
 };
 
-// функция для создания прелоадера
+// функция для создания прелоудера
 const createPreloader = () => {
   const preloader = document.createElement('div');
   preloader.className = 'preloader';
-  preloader.style.display = 'block'; // показываем прелоадер
+  preloader.style.display = 'block'; // показываем прелоудер
   return preloader;
 };
 
-const createImgElement = (url, imgDiv, preloader) => {
+// функция для создания и загрузки изображения
+const loadImage = (url, imgDiv, preloader) => {
   const img = document.createElement('img');
 
-  // когда изображение загрузится, скрываем прелоадер и добавляем img в imgDiv
+  // когда изображение загрузится, скрываем прелоудер и добавляем img в imgDiv
   img.onload = function () {
-    preloader.style.display = 'none'; // скрываем прелоадер
+    preloader.style.display = 'none'; // скрываем прелоудер
     imgDiv.style.height = 'auto'; // меняем высоту блока на auto
     imgDiv.appendChild(img); // добавляем img в imgDiv только после того, как изображение загрузилось
   };
@@ -58,7 +80,7 @@ const createImgElement = (url, imgDiv, preloader) => {
   // устанавливаем src изображения с задержкой в 2 секунды для тестирования прелоадера
   setTimeout(() => {
     img.src = url;
-  }, 212);
+  }, 2);
 };
 
 // функция для добавления блока изображения в галерею
@@ -72,24 +94,42 @@ const getRandomQuery = () => {
   return randomRequests[index];
 };
 
+// функция для очистки галереи
+const clearGallery = gallery => {
+  gallery.innerHTML = '';
+};
+
 // главная функция
 const main = async query => {
   const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=30&orientation=portrait&client_id=l-zkI308Tcihpn1AgexKwD_jFJ9SfU8I_008J48EBgg`;
 
   const images = await getData(url);
 
-  const gallery = document.querySelector('.galery__contents');
+  // очищаем галерею перед каждым запросом
+  clearGallery(gallery);
 
-  gallery.innerHTML = '';
+  const preloader = createPreloader();
+  gallery.appendChild(preloader);
+
+  // проверяем, есть ли изображения по данному запросу
+  if (images.length === 0) {
+    preloader.style.display = 'none'; // скрываем прелоудер
+    const noImagesOnRequest = document.createElement('p');
+    noImagesOnRequest.className = 'galery__text';
+    noImagesOnRequest.innerHTML =
+      'Unfortunately, no images were found for your request. Try another query. К сожалению, по вашему запросу не было найдено изображений. Попробуйте другой запрос.';
+    gallery.appendChild(noImagesOnRequest);
+    return;
+  }
 
   for (let index in images) {
-    await new Promise(resolve => setTimeout(resolve, index * 1));
     const image = images[index];
     const imgDiv = createImageDiv();
-    const preloader = createPreloader();
+    loadImage(image.urls.small, imgDiv, preloader);
     addImageDivToGallery(gallery, imgDiv);
-    createImgElement(image.urls.small, imgDiv, preloader);
-    imgDiv.appendChild(preloader);
+    if (index == '0') {
+      await new Promise(resolve => setTimeout(resolve, index * 20000));
+    }
   }
 };
 
